e2e435ed9bae52381092d32d5dfb84faa47cefbe
MostafaZayed <mostafa.zayed@hypercell.net>
2024-09-20 14:39:43 +0300

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CrisisListComponent } from "./components/crisis-list/crisis-list.component";
+import { HeroesListComponent } from "./components/heroes-list/heroes-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CrisisListComponent, HeroesListComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CrisisListComponent } from './components/crisis-list/crisis-list.component';
+import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'crisis-list', component: CrisisListComponent },
+    { path: 'heroes-list', component: HeroesListComponent },
+];
